72de3f283d0542559f35f4e5e7962b5cd7cccf8d
JuanPabloSarmiento <juanpnavarrete0@gmail.com>
2025-11-20 20:49:30 -0500

    Delete trazabilidad directory

--- trazabilidad/views/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['usuario_id'])){
-  header('Location: login.php'); exit;
-}
-?>
-<h1>Bienvenido al Dashboard</h1>
-<a href='../controllers/UsuarioController.php?logout=1'>Cerrar sesión</a>
--- trazabilidad/views/login.php
+++
@@ -1,7 +0,0 @@
-<form method='POST' action='../controllers/UsuarioController.php'>
-  <label>Email:</label>
-  <input type='email' name='email' required><br>
-  <label>Contraseña:</label>
-  <input type='password' name='password' required><br>
-  <button type='submit'>Ingresar</button>
-</form>
